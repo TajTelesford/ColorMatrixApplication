c8c3768a75972e00e69899ce9faf25976598156a
TajTe <TajTelesford@gmail.com>
2023-10-26 14:13:59 -0700

    Needs Work, Output Looks Cool - Custom PQueue

--- App.py
+++ App.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
+from queue import PriorityQueue, Queue
 from typing import Optional
 import pygame
 import random
-
+from CustomQueue import CustomQueue
 # Initialize Pygame
 pygame.init()
 
@@ -12,9 +13,9 @@ CELL_SIZE = 10
 MATRIX_WIDTH = WIDTH // CELL_SIZE
 MATRIX_HEIGHT = HEIGHT // CELL_SIZE
 TRAIL_LENGTH = 10  # Number of frames a character stays visible
-MAX_TRAILS = 20
-
-WHITE = (255, 255, 255)
+MAX_TRAILS = 150
+FPS = 20
+GREEN = (0, 255, 0)
 
 # ASCII characters
 ASCII_CHARACTERS = "023456789!@#$%^&*abcdefghijklmnopABCDEEFGHIJKLMNOP"
@@ -28,10 +29,12 @@ def display_grid(window):
     for i in range(0, WIDTH, CELL_SIZE):
         for j in range(0, HEIGHT, CELL_SIZE):
             rect = pygame.Rect(i, j, CELL_SIZE, CELL_SIZE)
-            pygame.draw.rect(window, WHITE, rect, 1 )
+            pygame.draw.rect(window, GREEN, rect, 1 )
 
 SET_OF_TRAILS = set()
-TRAILS = []
+
+trails = CustomQueue([], MAX_TRAILS)
+
 
 FONT = pygame.font.Font(None, 15)
 
@@ -43,85 +46,69 @@ class ListNode:
     maxed: Optional[bool] = False
 
 def append_node(node):
-    count = 0
-    prevPos = None
-    if not node.next:
-        
-        node.next = ListNode(
+    count = 1
+    prev = None
+    while node.next:
+        node = node.next
+        count += 1
+    maxed = count == TRAIL_LENGTH - 1 #Append a node with the maxed flag  == TRUE
+    node.next = ListNode(
             random.choice(ASCII_CHARACTERS),
             (node.pos[0], node.pos[1] - 10),
-            None
+            None,
+            maxed
         )
-        return
-    while node.next:
-        count += 1
-        prevPos = node.pos
         
-        node = node.next
-    if count < TRAIL_LENGTH:
-        node.next = ListNode(
-            random.choice(ASCII_CHARACTERS),
-            (prevPos[0], prevPos[1] - 10),
-            None
-        )
 
-    elif count == TRAIL_LENGTH:
-        node.maxed = True
-        
-    return None
 
 #This adds a Starting node for trails to grow
 def add_trails():
-    if len(SET_OF_TRAILS) < MAX_TRAILS:
-        x = random.randint(0, WIDTH)
-        while x in SET_OF_TRAILS:
-            x = random.randint(0, WIDTH)
-            if x not in SET_OF_TRAILS:
-                node = ListNode(
-                    random.choice(ASCII_CHARACTERS),
-                    (x, random.randint(0, HEIGHT)),
-                    None
-                )
-                SET_OF_TRAILS.add(node.pos[0]) #Adds the x value into the set so we won't have overlapping trails
-                TRAILS.append(node) #adds the node so we could start appending nodes to this trail
+    x = random.randint(0, WIDTH - 1)
+    while x not in trails.xSet():
+        x = random.randint(0, WIDTH - 1)
+    y = random.randint(0, HEIGHT  - 1)
+    trails.addX(x)
+
+    return ListNode(
+        random.choice(ASCII_CHARACTERS),
+        (x, y),
+        None
+    )
+            
 
 def draw_trails(trail: ListNode):
     while trail:
-        
-        char = FONT.render(trail.value, True, WHITE)
+        char = FONT.render(trail.value, True, GREEN)
         screen.blit(char, trail.pos)
         trail = trail.next
 
+
 def move_trails():
-    idx = 0
-    for node in TRAILS:
-        if not node.next and node.maxed: #if their next node is null, we will remove the x position from the set and add the start of a new trail
-            print("max")
-            SET_OF_TRAILS.remove(node.pos[0])
-            TRAILS.pop(idx)
-            add_trails()
-            continue
-        elif node.pos[1] > HEIGHT: #If node is greater then Height we will delete it from the linked list
-            #print("HERE")
-            SET_OF_TRAILS.remove(node.pos[0])
-            if node.next:
-                SET_OF_TRAILS.add(node.next.pos[0])
-                #print("HERE")
-                node = node.next
-        idx += 1
-        n = None
+
+    for node in trails.Trails():
         temp = node
-        if temp:
-            n = append_node(temp)
-        if n:
-            node.next = n
-        temp = node
+        append_node(temp)
+        
+        prev = None
         while temp:
-            temp.pos = (temp.pos[0], temp.pos[1] + 5)
+            temp.pos = (temp.pos[0], temp.pos[1] + 15)
+            prev = temp
             temp = temp.next
+        if prev and prev.maxed:
+            trails.dequeue()
+            trails.enqueue(add_trails())
 
 def update_trails():
-    for node in TRAILS:
+        
+    for node in trails.Trails():
+        if node.pos[1] > HEIGHT: #If node is greater then Height we will delete it from the linked list
+            trails.Pop(node, add_trails)
+            new = add_trails()
+            trails.removeX(node.pos[0])
+            trails.enqueue(new)
+            trails.addX(node.pos[0])
+            if node.next:
+                node = node.next
         temp = node
         draw_trails(temp)
     move_trails()
@@ -129,19 +116,16 @@ def update_trails():
 def initialize_trails():
     for _ in range(MAX_TRAILS):
         x = random.randint(0, WIDTH)
-        while x not in SET_OF_TRAILS:
-            x = random.randint(0, WIDTH)
-            if x not in SET_OF_TRAILS:
-                break
+        if x in trails.xSet():
+            while x not in trails.xSet():
+                x = random.randint(0, WIDTH)
         y = random.randint(0, HEIGHT)
-        TRAILS.append(
-            ListNode(
+        trails.enqueue(ListNode(
                 random.choice(ASCII_CHARACTERS),
                 (x, y),
                 None
-            )
-        )
-        SET_OF_TRAILS.add(x)
+            ))
+        trails.addX(x)
 
 def main():
     frame = 0  # Frame counter
@@ -153,13 +137,10 @@ def main():
                 running = False
         
         screen.fill((0, 0, 0))  
-       
         update_trails()
         pygame.display.update()
-        print(SET_OF_TRAILS)
-        #print(TRAILS)
         frame += 1
-        clock.tick(30)  # Adjust the frame rate as needed
+        clock.tick(FPS)  # Adjust the frame rate as needed
 
     pygame.quit()
 
---
+++ CustomQueue.py
@@ -0,0 +1,81 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class CustomQueue:
+    items: list
+    MAXSIZE: int
+    x_values = set()
+
+    def xSet(self) -> set:
+        if not self.x_values:
+            for items in self.items:
+                self.x_values.add(items.pos[0])
+        return self.x_values
+    
+    def removeX(self, x):
+        if x in self.x_values:
+            self.x_values.remove(x)
+
+    def addX(self, x):
+        self.x_values.add(x)
+
+    def isEmpty(self) -> bool:
+        return len(self.items) == 0
+    
+    def enqueue(self, item):
+        if self.MAXSIZE == len(self.items):
+            return 
+        
+        if not self.isEmpty():
+            self.items.append(item)
+            self.Sort()
+        else: 
+            self.items.append(item)
+
+    def dequeue(self):
+        if self.isEmpty():
+            return 
+        item = self.Top()
+        self.items.pop(0)
+        return item
+    
+    def Top(self):
+        return self.items[0]
+
+    def Sort(self):
+        for i in range(len(self.items)):
+            idx = i
+            for j in range(len(self.items)):
+                if self.items[idx].pos[1] < self.items[j].pos[1]:
+                    idx = j
+            self.items[i], self.items[idx] = self.items[idx], self.items[i]
+
+    def Pop(self, node, add):
+        for i in range(len(self.items)):
+            if self.items[i].pos == node.pos:
+                self.items[i] = add()
+
+    def check(self, height):
+        if self.Top().pos[1] >= height and self.Top().maxed:
+            return self.dequeue()
+        if self.Top().pos[1] >= height and self.Top().next:
+            item = self.Top()
+            item = item.next
+        return None
+        
+
+
+    def UpdateNodes(self, appendFunc):
+        for item in self.items:
+            node = item
+            while node:
+                node.pos = (node.pos[0], node.pos[1] + 5)
+                node = node.next
+        
+        for item in self.items:
+            node = item
+            appendFunc(node)
+    
+    def Trails(self) -> list:
+        return self.items
